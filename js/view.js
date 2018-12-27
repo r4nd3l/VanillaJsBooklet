de7fc6251b1f77631d63e7f86fd7c9a91ad6b3fe
matemolnar88 <matemolnar88@gmail.com>
2018-12-27 14:20:30 +0100

    Organised folder structure and Dev Links section added

--- js/view.js
+++ js/view.js
@@ -11,14 +11,16 @@ function navBar(){
 }
 
 $(document).ready(function mainPage(){
-  $('#show').fadeOut(300, function(){$('#show').load("tasks/home.html", function(){$('#show').fadeIn(300);});});
+  $('#show').fadeOut(300, function(){$('#show').load("sections/main/home.html", function(){$('#show').fadeIn(300);});});
 
-  $('#home').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/home.html", function(){$('#show').fadeIn(300);});});});
-  $('#variables').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/variables.html", function(){$('#show').fadeIn(300);});});});
-  $('#arrays').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/arrays.html", function(){$('#show').fadeIn(300);});});});
-  $('#loops').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/loops.html", function(){$('#show').fadeIn(300);});});});
-  $('#conditionals').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/conditionals.html", function(){$('#show').fadeIn(300);});});});
-  $('#objects').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/objects.html", function(){$('#show').fadeIn(300);});});});
-  $('#events').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/events.html", function(){$('#show').fadeIn(300);});});});
-  $('#tasks').click(function(){$('#show').fadeOut(300, function(){$('#show').load("tasks/tasks.html", function(){$('#show').fadeIn(300);});});});
+  $('#home').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/main/home.html", function(){$('#show').fadeIn(300);});});});
+  $('#variables').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/instances/variables.html", function(){$('#show').fadeIn(300);});});});
+  $('#arrays').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/instances/arrays.html", function(){$('#show').fadeIn(300);});});});
+  $('#loops').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/instances/loops.html", function(){$('#show').fadeIn(300);});});});
+  $('#conditionals').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/instances/conditionals.html", function(){$('#show').fadeIn(300);});});});
+  $('#objects').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/instances/objects.html", function(){$('#show').fadeIn(300);});});});
+  $('#events').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/instances/events.html", function(){$('#show').fadeIn(300);});});});
+  $('#tasks').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/tasks/tasks.html", function(){$('#show').fadeIn(300);});});});
+  $('#dev_corner').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/dev/dev_corner.html", function(){$('#show').fadeIn(300);});});});
+  $('#dev_links').click(function(){$('#show').fadeOut(300, function(){$('#show').load("sections/dev/dev_links.html", function(){$('#show').fadeIn(300);});});});
 });
